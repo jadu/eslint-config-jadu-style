cf181d783441681960ad654a748a2c2afb48c59c
Paul Bele <paul.bele@jadu.net>
2019-10-16 15:12:18 +0300

    initial version of jadu-style

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-
+module.exports = {
+  extends: 'airbnb-base',
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true
+  },
+  rules: {
+    'no-param-reassign': [2, { 'props': false }],
+    'no-shadow': ['error', { 'allow': ['state', 'modules']}],
+    'no-empty': ['error', { 'allowEmptyCatch': true }]
+  }
+};
